3a1810138fa7ef103b2335fd93c5a5556acc428c
shut-up <936097245@qq.com>
2017-10-07 12:48:30 +0800

    异常处理开发

---
+++ src/cn/parker/ssm/exception/CustomExceptionResolver.java
@@ -0,0 +1,32 @@
+package cn.parker.ssm.exception;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.servlet.HandlerExceptionResolver;
+import org.springframework.web.servlet.ModelAndView;
+
+public class CustomExceptionResolver implements HandlerExceptionResolver{
+
+	@Override
+	public ModelAndView resolveException(HttpServletRequest request, HttpServletResponse response, Object object,
+			Exception ex) {
+		CustomException customException = null;
+		if(ex instanceof CustomException){
+			customException = (CustomException) ex;
+		}else{
+			customException = new CustomException("未知错误");
+		}
+		
+		String message = customException.getMessage();
+		
+		ModelAndView modelAndView = new ModelAndView();
+		
+		modelAndView.addObject("message", message);
+		
+		modelAndView.setViewName("error");
+		
+		return modelAndView;
+	}
+
+}
