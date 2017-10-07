87202f2311a743b10595ecf4945bb038328b6896
shut-up <936097245@qq.com>
2017-10-07 23:46:17 +0800

    拦截器的定义与配置

---
+++ src/cn/parker/ssm/interceptor/handlerInterceptor1.java
@@ -0,0 +1,46 @@
+package cn.parker.ssm.interceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+public class handlerInterceptor1 implements HandlerInterceptor{
+
+	
+	//进入handler之前执行
+	//用于身份认证、身份授权
+	//比如身份认证，如果认证通过表示当前用户没有登陆，需要此方法拦截不再向下执行
+	@Override
+	public boolean preHandle(HttpServletRequest arg0, HttpServletResponse arg1, Object arg2) throws Exception {
+
+		
+		System.out.println("handlerInterceptor1...preHandle");
+		
+		//return false;表示拦截成功
+		return true;
+	}
+	
+	//进入Handler后，返回ModelAndView之前执行
+	//应用场景从modelAndView出发：将公用的模型数据(比如菜单导航)在这里传到视图，也可以在这里统一指定视图
+	@Override
+	public void postHandle(HttpServletRequest arg0, HttpServletResponse arg1, Object arg2, ModelAndView arg3)
+			throws Exception {
+
+		System.out.println("handlerInterceptor1...postHandle");
+		
+	}
+
+	//执行Handler完成执行此方法
+	//应用场景：统一异常处理，统一日志处理
+	@Override
+	public void afterCompletion(HttpServletRequest arg0, HttpServletResponse arg1, Object arg2, Exception arg3)
+			throws Exception {
+
+		System.out.println("handlerInterceptor1...afterCompletion");
+		
+	}
+
+
+}
